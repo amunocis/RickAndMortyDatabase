c55e6b207a7b884d56d5af962aa2f48cd354836f
amunocis <amunocis@gmail.com>
2021-03-08 21:13:00 -0300

    feat: Character item animated from outside

--- app/src/main/java/com/example/rickmorty/view/adapter/CharacterAdapter.kt
+++ app/src/main/java/com/example/rickmorty/view/adapter/CharacterAdapter.kt
@@ -1,13 +1,16 @@
 package com.example.rickmorty.view.adapter
 
+import android.content.Context
 import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AnimationUtils
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.example.rickmorty.R
 import com.example.rickmorty.databinding.CharacterItemBinding
 import com.example.rickmorty.model.local.entities.Character
 
@@ -15,6 +18,9 @@ class CharacterAdapter: RecyclerView.Adapter<CharacterAdapter.CharacterVH>() {
 
     private var listCharacter = listOf<Character>()
     private val selectedCharacter = MutableLiveData<Character>()
+
+    lateinit var context: Context
+    private var lastPosition = -1
 
     fun update(list: List<Character>) {
         listCharacter = list
@@ -25,6 +31,7 @@ class CharacterAdapter: RecyclerView.Adapter<CharacterAdapter.CharacterVH>() {
 
     inner class CharacterVH(private val binding: CharacterItemBinding):
         RecyclerView.ViewHolder(binding.root), View.OnClickListener {
+        var animation = AnimationUtils.loadAnimation(context, R.anim.slide_from_outside)
 
         fun bind(character: Character) {
             binding.tvCharacter.text = character.name
@@ -36,6 +43,7 @@ class CharacterAdapter: RecyclerView.Adapter<CharacterAdapter.CharacterVH>() {
                 binding.ivFav.setColorFilter(Color.LTGRAY)
             }
             itemView.setOnClickListener(this)
+            itemView.startAnimation(animation)
         }
         override fun onClick(v: View?) {
             selectedCharacter.value = listCharacter[adapterPosition]
@@ -43,11 +51,16 @@ class CharacterAdapter: RecyclerView.Adapter<CharacterAdapter.CharacterVH>() {
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CharacterVH {
+        context = parent.context
         return CharacterVH(CharacterItemBinding.inflate(LayoutInflater.from(parent.context)))
     }
 
     override fun onBindViewHolder(holder: CharacterVH, position: Int) {
-        holder.bind(listCharacter[position])
+        if (holder.adapterPosition > lastPosition) {
+            holder.bind(listCharacter[position])
+            lastPosition = holder.adapterPosition
+        }
+
     }
 
     override fun getItemCount() = listCharacter.size
